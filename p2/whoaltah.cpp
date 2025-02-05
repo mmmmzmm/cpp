5ad147296f36101aee32b467e83f50a210ac3d1c
mmmmzmm <kisstfm1@gmail.com>
2025-02-05 22:01:00 +0100

    whoaltah

--- p2/whoaltah.cpp
+++ p2/whoaltah.cpp
@@ -1,5 +1,31 @@
-//
-// Created by Komputer on 05.02.2025.
-//
+#include <iostream>
+using namespace std;
 
-#include "whoaltah.h"
+
+int main() {
+
+    int x;
+
+    cout << "Choose a number (0-9) to determine if its odd or even" << '\n';
+    cin >> x;
+
+    switch (x) {
+        case 1:
+            case 3:
+        case 5:
+        case 7:
+        case 9:
+        cout << "The number is odd";
+        break;
+        case 2:
+            case 4:
+        case 8:
+        case 0:
+        cout << "The number is even";
+        break;
+        default:
+            cout << "Incorrect input";
+    }
+
+    return 0;
+}
