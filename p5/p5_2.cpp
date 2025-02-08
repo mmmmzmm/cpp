8d5828c5903888a9e15502d3dea73af55bab118e
mmmmzmm <kisstfm1@gmail.com>
2025-02-08 15:39:55 +0100

    whitney wisconsin?

--- p5/p5_2.cpp
+++ p5/p5_2.cpp
@@ -5,40 +5,40 @@ int main (){
   //testing letter case
   //i know i could cout under every question but i just wanted to do  it at the end
 
-    char x;
-    char y;
-    int z;
+    char size;
+    char gender;
+    int amount;
 
     cout << "What's your size? (S/M/L)" << '\n';
-    cin >> x;
-    if (islower(x)) {
-      x = x - 32;
+    cin >> size;
+    if (islower(size)) {
+      size = size - 32;
     }
 
     cout << "Are you a male or a female? (M/F)" << '\n';
-    cin >> y;
-    if (islower(y)) {
-      y = y - 32;
+    cin >> gender;
+    if (islower(gender)) {
+      gender = gender - 32;
     }
 
     cout << "How many do you need?" << '\n';
-    cin >> z;
+    cin >> amount;
 
-    if (x == 'S' || x == 'L') {
+    if (size == 'S' || size == 'L') {
       cout << "The size is available" << '\n';
     }
     else {
       cout << "The size is not available" << '\n';
     }
 
-    if (y == 'M') {
+    if (gender == 'M') {
       cout << "Men's clothing is in stock" << '\n';
     }
     else {
       cout << "Female's clothing is out of stock." << '\n';
     }
 
-    if (z < 10) {
+    if (amount < 10) {
       cout << "This amount is available." << '\n';
     }
     else {
