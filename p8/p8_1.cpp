337f78d80c5fc0f3b64403a911e8d6490d4fd816
mmmmzmm <kisstfm1@gmail.com>
2025-02-15 12:39:09 +0100

    walter woofie?

---
+++ p8/p8_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+//number guessing game
+
+    int num;
+    int guess;
+    int tries = 0;
+    string answer;
+
+    srand(time(NULL));
+    num = rand() % 10 + 1;
+
+    cout << "Guess a random number between 1-10: ";
+    cin >> guess;
+
+
+    do {
+        if (guess > num) {
+            cout << "Too high. Try again: ";
+            tries = tries + 1;
+            cin >> guess;
+        }
+        else if (guess < num) {
+            cout << "Too low. Try again: ";
+            tries = tries + 1;
+            cin >> guess;
+        }
+    }while (guess != num);
+
+    if (guess == num){
+        cout << "You guessed it! It took you " << tries << " tries!" << '\n';
+    }
+
+
+
+    return 0;
+}
